954cb0a03500bd65803e1abb47fa4a74f0688d01
mickeljilenko123 <mickeljilenko@gmail.com>
2020-07-14 18:39:27 +0300

    Js 4.1

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-/* Этот код менять не нужно */
-const defaultUserName = 'anonymus';
-const userName = 'John';
-const anotherUser = null;
-const isAdmin = false;
-const message = 'text';
-const anotherMessage = 'text as well';
-const isLoggedIn = Boolean(userName);
-
-/* В коде ниже нужно использовать правильные операторы после знака присваивания */
-const activeUser = anotherUser || defaultUserName;
-console.log(activeUser);
-const hasAccess = isAdmin || isLoggedIn;
-console.log(hasAccess);
-const isTruthy = message !== anotherMessage;
-console.log(isTruthy);
-const isFalsy = !isLoggedIn;
-console.log(isFalsy);
-const isNotTrue = typeof String(message) == 'number';
-console.log(isNotTrue);
-const isTrue = typeof Boolean(message) == 'boolean';
-console.log(isTrue);
+let n = 125;
+let sum = 0;
+while (n > 0) {
+    sum += n;
+    n--;
+}
+console.log('Result: ' + sum);

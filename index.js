3f4edcd4cfa5390f88c7b092c8a0d35ab5034f9d
mickeljilenko123 <mickeljilenko@gmail.com>
2020-07-15 18:56:11 +0300

    Js 4.5/1

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-let m = 1;
-let sum = 1;
-const n = 20;
-while (m < 20) {
-    m += 1;
-    sum += String(m);
+const n = 40;
+let result = 0;
+
+for (let m = 10; m <= 20; m++) {
+    if (m % 2 === 0 && m % 4 !== 0) {
+        result += m;
+    } else if (m % 3 === 0) {
+        result -= m;
+    } else if (m % 4 === 0) {
+        result *= m;
+    } else if (m % 5 === 0) {
+        console.log(m);
+    }
 
 }
-console.log('Result: ' + sum);
+
+console.log('Result: ' + result);

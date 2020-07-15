d41e97d67aef9dfb20150804581a1b7e7c5354af
mickeljilenko123 <mickeljilenko@gmail.com>
2020-07-15 22:06:20 +0300

    Js 4.4.10

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ do {
         result -= a;
     } else if (a % 4 === 0) {
         result *= a;
-    } else if (a % 5 === 0) {
-        result += a;
+    }
+    if (a % 5 === 0) {
+
         console.log(a);
     }
     a++;

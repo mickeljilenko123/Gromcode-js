d76b73e8d117d4f95e0177c4cf3805ab7c1ae550
mickeljilenko123 <mickeljilenko@gmail.com>
2020-07-15 15:17:42 +0300

    Js 4.3/2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 for (let i = 1; i <= 5; i++) {
     console.log('\n=> ' + i + ' <=\n\n'); //Таблица умножение 5 колонок
     for (let j = 1; j <= 10; j++) { // Для строк
-        console.log(i + ' * ' + j + ' = ' + i * j);
+        console.log(i + ' x ' + j + ' = ' + i * j);
     }
 }

50a2cb12515a60948ca31e3bee5694e20b9c908e
mickeljilenko123 <mickeljilenko@gmail.com>
2020-07-15 23:40:38 +0300

    JS 4.4.10

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const m = 2;
-const n = 25;
+const n = 50;
 let result = 0;
-
-for (let i = m; i <= n; i++) {
-    if (i % 2 === 0 && i % 4 !== 0) {
-        result += i;
-
-    } else if (i % 3 === 0) {
-        result -= i;
-
-    } else if (i % 4 === 0) {
-        result *= i;
+for (let a = m; a <= n; a++) {
+    if (a % 2 === 0 && a % 4 !== 0) {
+        result += a;
+    } else if (a % 3 === 0) {
+        result -= a;
+    } else if (a % 4 === 0) {
+        result *= a;
 
     }
-    if (i % 5 === 0) {
-        console.log(i);
+    if (a % 5 === 0) {
+        console.log(a);
     }
+    result = a;
 }

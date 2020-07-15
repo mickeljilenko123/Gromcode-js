c2023ada02ebe3267474135f2bf0e62ed71849a4
mickeljilenko123 <mickeljilenko@gmail.com>
2020-07-15 23:36:23 +0300

    JS 4.4.14

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-const m = 10;
-const n = 48;
-let a = 10;
-let sum = 0;
+const m = 2;
+const n = 25;
 let result = 0;
-for (a = 10; a <= n; a++) {
-    if (a % 2 === 0 && a % 4 !== 0) {
-        sum += a;
-    }
-    if (a % 3 === 0) {
-        sum -= a;
-    }
-    if (a % 4 === 0) {
-        sum *= a;
+
+for (let i = m; i <= n; i++) {
+    if (i % 2 === 0 && i % 4 !== 0) {
+        result += i;
+
+    } else if (i % 3 === 0) {
+        result -= i;
+
+    } else if (i % 4 === 0) {
+        result *= i;
 
     }
-    if (a % 5 === 0) {
-        console.log(a);
+    if (i % 5 === 0) {
+        console.log(i);
     }
-    sum = result;
 }

6eab5b45980bd9d11023c7ae2c6276bfc5f23bfa
mickeljilenko123 <mickeljilenko@gmail.com>
2020-07-15 22:39:16 +0300

    Js 4.4.17

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const m = 10;
 const n = 48;
-let a = m;
+let a = 10;
 let result = 0;
 
-for (a = m; a <= n; a++) {
+for (a = 10; a <= n; a++) {
     if (a % 2 === 0 && a % 4 !== 0) {
         result += a;
-    } else if (a % 3 === 0) {
+    }
+    if (a % 3 === 0) {
         result -= a;
-    } else if (a % 4 === 0) {
+    }
+    if (a % 4 === 0) {
         result *= a;
     }
     if (a % 5 === 0) {
         console.log(a);
-        result += a;
     }
 }
-console.log('Result: ' + result);
+result = a;

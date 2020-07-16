e220d0ca963793686fe8d9577561a3cc3a7928ec
mickeljilenko123 <mickeljilenko@gmail.com>
2020-07-16 23:29:12 +0300

    Js 4.6/2

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 let result = 0;
 
-for (let n = 0; n <= 1000; n++) {
-    if (n % 2 === 1) {
-        console.log('Found');
-        continue;
-    }
+for (let n = 0; n <= 10000000; n++) {
+
     result += n;
 }
-
-if (result * 5 > 5000) {
-    console.log('Bigger');
-} else {
-    console.log("Smaller or equal");
-}
+console.log(result);

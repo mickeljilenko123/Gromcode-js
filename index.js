144c51c2068f3f916f382cce58a65dd6a065dd38
mickeljilenko123 <mickeljilenko@gmail.com>
2020-07-17 20:24:14 +0300

    Js 5.5/1

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const square = num => num * num
-console.log(square(2));
+let result = 0;
+
+function getSum(m, n) {
+    for (let i = m; i <= n; i++) {
+        if (i % 2 === 0) {
+            result += i;
+        }
+    }
+}
+getSum(10, 30);
+console.log(result);

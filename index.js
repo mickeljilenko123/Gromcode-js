9964bb324c0874d6e0565a13b794d9e85b9cce98
mickeljilenko123 <mickeljilenko@gmail.com>
2020-07-17 20:47:08 +0300

    Js 5.5/5

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ function getSum(m, n) {
     for (let i = m; i <= n; i++) {
         if (i % 2 === 0) {
             result += i;
-            continue;
         }
     }
+    console.log(result);
 }
 getSum(2, 12);
-console.log('Sum: ' + result);

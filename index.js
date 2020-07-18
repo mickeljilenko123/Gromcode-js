51822c1dbcbeba155d7a2fadfbd6928befcca0e3
mickeljilenko123 <mickeljilenko@gmail.com>
2020-07-18 15:34:51 +0300

    Js 5.6/8

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function getPrimes(n) {
     nextPrimes: for (let i = 2; i <= n; i++) {
         for (let j = 2; j < i; j++) {
-            if (i % j === 0) {
+            if (i % j == 0) {
                 continue nextPrimes;
             }
         }

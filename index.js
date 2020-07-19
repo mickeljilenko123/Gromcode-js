a262a70fead50a1f187b71974080d78d15f86ab0
mickeljilenko123 <mickeljilenko@gmail.com>
2020-07-19 10:23:51 +0300

    Js 5.8/1

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-function sum(from, to) {
-    let total = 0;
-    for (let i = from; i <= to; i++) {
-        total += i;
-    }
-    return total;
-}
+function findDivCount(a, b, n) {
+    for (let i = a; i < b; i++) {
+        if (i % n === 0) {
+            while (i < n) {
+                i++;
+            }
+        }
+        return i;
 
-function compareSums(a, b, c, d) {
-    if (sum(a, b) > sum(c, d)) {
-        return true;
-    } else {
-        return false;
     }
+
 }
-compareSums(2, 9, 1, 2)
+console.log(findDivCount(2, 9, 3));
